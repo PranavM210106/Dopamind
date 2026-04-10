df72735ca995788d08ff2f5a366802ae618c6b56
PranavM210106 <mehtapranav048@gmail.com>
2026-04-10 20:28:56 +0530

    Fix casing and remove unused import

--- my-app/src/pages/client/profile.tsx
+++ my-app/src/pages/client/profile.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
 import Sidebar from "../../components/Sidebar"
 import Navbar from "../../components/Navbar"
 import { getProfile, updateProfile } from "../../api/api"
